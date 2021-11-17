0d9ed73cf32e6b941885156b5bf77212460ee4c2
Yato Aki <yatoakii@gmail.com>
2021-11-17 15:57:24 +0630

    Added new cat interface

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,4 +9,23 @@ class CatsController < ApplicationController
         render :show
     end
 
+    def new 
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date,:color,:name,:sex,:description)
+    end
 end 
