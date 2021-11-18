2a583f9df53d970b4c610f5462ea6336b72b38d5
Yato Aki <yatoakii@gmail.com>
2021-11-18 15:47:24 +0630

    Can accept and deny rental request now

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,5 +1,15 @@
 class CatRentalRequestsController < ApplicationController
 
+    def approve
+        current_cat_rental_request.approve!
+        redirect_to cat_url(current_cat_rental_request.cat)
+    end
+
+    def deny
+        current_cat_rental_request.deny!
+        redirect_to cat_url(current_cat_rental_request.cat)
+    end
+
     def new
         render :new
     end
@@ -15,6 +25,11 @@ class CatRentalRequestsController < ApplicationController
     end 
 
     private 
+
+    def current_cat_rental_request
+        @cat_rental_request ||=
+          CatRentalRequest.includes(:cat).find(params[:id])
+    end
 
     def safe_params
         params.require(:cat_rental_request)
--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -6,6 +6,26 @@ class CatRentalRequest < ActiveRecord::Base
 
     belongs_to :cat
 
+    def approve!
+        raise "The request is not pending" unless self.status == 'PENDING'
+        transaction do
+            self.status = 'APPROVED'
+            self.save!
+            overlapping_pending_requests.each do |req|
+                req.update!(status: 'DENIED')
+            end
+        end
+    end
+
+    def pending?
+        self.status == 'PENDING'
+    end
+
+    def deny!
+        self.status = 'DENIED'
+        self.save!
+    end
+
     def overlapping_requests
         CatRentalRequest
         .where.not(id: self.id)
@@ -15,6 +35,10 @@ class CatRentalRequest < ActiveRecord::Base
 
     def overlapping_approved_requests
         overlapping_requests.where('status = \'APROVED\'')
+    end
+
+    def overlapping_pending_requests
+        overlapping_requests.where('status = \'PENDING\'')
     end
 
     def does_not_overlap_approved_request
